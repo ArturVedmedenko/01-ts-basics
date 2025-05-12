3fce75dfe8b01a4b1aa32393df00fcc7ca7da02a
Artur Vedmedenko <vedmedenko.a@gmail.com>
2025-05-12 22:04:53 +0300

    Initial commit

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-const app = document.getElementById("app");
-
-if (app) {
-  app.innerHTML = "<h1>Hello TypeScript + Vite!</h1>";
-  console.log("App initialized");
-} else {
-  console.error('Element with id "app" not found');
-}
